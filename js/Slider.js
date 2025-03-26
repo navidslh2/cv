81f60826e8452985934c1a5eb3a7899667b5d2aa
Navid Salehi <52621302+navidslh2@users.noreply.github.com>
2025-03-26 19:40:55 +0330

    get data from sql

---
+++ js/Slider.js
@@ -0,0 +1,18 @@
+const Slider=async()=>{
+    try{
+        let data=await fetch("http://localhost:3000/slider")
+        let res=await data.json()
+        let slider=res.map(elem=>{
+            return `<div class="swiper-slide">
+                        <img src="${elem.image}" alt="${elem.alt}">
+                    </div>` 
+        })
+        document.querySelector(".swiper-wrapper").innerHTML=slider.join("")
+    }catch(error){
+        console.log(error)
+    }
+    
+}
+
+export default Slider
+
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import Slider from "./Slider.js";
+
+
+Slider()
