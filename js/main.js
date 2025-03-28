3dd8e8f1256c3d6e5c0cb70bb821888b42c2dce7
Navid Salehi <52621302+navidslh2@users.noreply.github.com>
2025-03-28 18:19:45 +0330

    add swiper html with js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+import slide from "./abilities.js";
+import "./bootstarp.js"
+import "./fontawesome/all.js"
+
+
+slide()
+
+/* start swiper js code */
+
+
+  
+  /* finish swiper js code */
+
+
+
