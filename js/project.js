7641bfdcd6038f8c1c70fe76a81379be3c04ef02
Navid Salehi <52621302+navidslh2@users.noreply.github.com>
2025-03-29 12:43:40 +0330

    add swiper html with js

--- js/project.js
+++ js/project.js
@@ -6,7 +6,34 @@ let btnGit = document.getElementById('btnGit');
 
 function showCode(project) {
     let codes = {
-        "project1" : `123`,
+        "project1" : `const time=document.querySelector(".time")
+const hour=document.querySelectorAll(".controler>select")[0]
+const minutes=document.querySelectorAll(".controler>select")[1]
+const button=document.querySelector("button")
+const ringTone=new Audio("./audio/ringtone.mp3")
+let alarm=null
+setInterval(()=>{
+    let date=new Date()
+    time.innerHTML=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    if(alarm==`${date.getHours()}:${date.getMinutes()}`){
+        ringTone.play()
+    }
+},100)
+
+for(let i=1;i<25;i++){
+    hour.innerHTML+=`<option value="${i}">${i}</option>`
+}
+
+for(let i=1;i<61;i++){
+    minutes.innerHTML+=`<option value="${i}">${i}</option>`
+}
+
+button.addEventListener("click",()=>{
+    alarm=`${hour.value}:${minutes.value}`
+    if(alarm.includes("hour") || alarm.includes("hour") ){
+        return alert("select valid time")
+    }
+})`,
         "project2" : [`       import os 
                 from datetime import datetime
                 def creat_datefile(file_path):
